29305cacd0388800ec097e5672e48cb60f1650ef
aditya1183 <adityasharma44159@gmail.com>
2024-11-22 11:46:31 +0530

    add update listing and eit api response

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,45 +1,44 @@
-import Listing from '../models/listing.model.js';
-import { errorHandler } from '../utils/error.js';
+import { console } from "inspector";
+import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
-// export const createListing = async (req, res, next) => {
-//   try {
-//     const listing = await Listing.create(req.body);
-//     return res.status(201).json(listing);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
-
-
-export  const createListing = async (req, res, next) => {
+export const createListing = async (req, res, next) => {
   try {
-    const { imageUrls, ...listingData } = req.body;
-    const listing = await Listing.create({
-      ...listingData,
-      images: imageUrls, // Store URLs in the database
-    });
-
-    res.status(201).json(listing);
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);
   } catch (error) {
     next(error);
   }
 };
 
+// export  const createListing = async (req, res, next) => {
+//   try {
+//     const { imageUrls, ...listingData } = req.body;
+//     const listing = await Listing.create({
+//       ...listingData,
+//       images: imageUrls, // Store URLs in the database
+//     });
+
+//     res.status(201).json(listing);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
   if (!listing) {
-    return next(errorHandler(404, 'Listing not found!'));
+    return next(errorHandler(404, "Listing not found!"));
   }
 
   if (req.user.id !== listing.userRef) {
-    return next(errorHandler(401, 'You can only delete your own listings!'));
+    return next(errorHandler(401, "You can only delete your own listings!"));
   }
 
   try {
     await Listing.findByIdAndDelete(req.params.id);
-    res.status(200).json('Listing has been deleted!');
+    res.status(200).json("Listing has been deleted!");
   } catch (error) {
     next(error);
   }
@@ -47,11 +46,14 @@ export const deleteListing = async (req, res, next) => {
 
 export const updateListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
+  console.log(req.body);
+
   if (!listing) {
-    return next(errorHandler(404, 'Listing not found!'));
+    return next(errorHandler(404, "Listing not found!"));
   }
-  if (req.user.id !== listing.userRef) {
-    return next(errorHandler(401, 'You can only update your own listings!'));
+
+  if (req.user.id !== listing.userRef.toString()) {
+    return next(errorHandler(401, "You can only update your own listings!"));
   }
 
   try {
@@ -66,11 +68,47 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
+// export const updateListing = async (req, res, next) => {
+//   try {
+//     const listing = await Listing.findById(req.params.id);
+//     console.log(listing);
+
+//     if (!listing) {
+//       return next(errorHandler(404, "Listing not found!"));
+//     }
+
+//     if (req.user.id !== listing.userRef.toString()) {
+//       return next(errorHandler(401, "You can only update your own listings!"));
+//     }
+
+//     const updatedFields = { ...req.body }; // Ensure all fields from req.body are captured
+
+//     const updatedListing = await Listing.findByIdAndUpdate(
+//       req.params.id,
+//       {
+//         $set: updatedFields,
+//         name: updatedFields.name,
+//         imageUrls: updatedFields.imageUrls,
+//       }, // Use $set to replace arrays explicitly
+//       { new: true, runValidators: true } // Return updated document & enforce schema validation
+//     );
+
+//     if (!updatedListing) {
+//       return next(errorHandler(500, "Failed to update the listing!"));
+//     }
+
+//     res.status(200).json(updatedListing);
+//   } catch (error) {
+//     console.error("Error updating listing:", error);
+//     next(error);
+//   }
+// };
+
 export const getListing = async (req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
     if (!listing) {
-      return next(errorHandler(404, 'Listing not found!'));
+      return next(errorHandler(404, "Listing not found!"));
     }
     res.status(200).json(listing);
   } catch (error) {
@@ -84,36 +122,36 @@ export const getListings = async (req, res, next) => {
     const startIndex = parseInt(req.query.startIndex) || 0;
     let offer = req.query.offer;
 
-    if (offer === undefined || offer === 'false') {
+    if (offer === undefined || offer === "false") {
       offer = { $in: [false, true] };
     }
 
     let furnished = req.query.furnished;
 
-    if (furnished === undefined || furnished === 'false') {
+    if (furnished === undefined || furnished === "false") {
       furnished = { $in: [false, true] };
     }
 
     let parking = req.query.parking;
 
-    if (parking === undefined || parking === 'false') {
+    if (parking === undefined || parking === "false") {
       parking = { $in: [false, true] };
     }
 
     let type = req.query.type;
 
-    if (type === undefined || type === 'all') {
-      type = { $in: ['sale', 'rent'] };
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
     }
 
-    const searchTerm = req.query.searchTerm || '';
+    const searchTerm = req.query.searchTerm || "";
 
-    const sort = req.query.sort || 'createdAt';
+    const sort = req.query.sort || "createdAt";
 
-    const order = req.query.order || 'desc';
+    const order = req.query.order || "desc";
 
     const listings = await Listing.find({
-      name: { $regex: searchTerm, $options: 'i' },
+      name: { $regex: searchTerm, $options: "i" },
       offer,
       furnished,
       parking,
--- api/index.js
+++ api/index.js
@@ -6,8 +6,7 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
-import {cloudinaryConnect} from "./config/cloudinary.js";
-
+import { cloudinaryConnect } from "./config/cloudinary.js";
 
 dotenv.config();
 
@@ -29,6 +28,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
