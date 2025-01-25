25d370c6509af48cad49a58875b4b16bb50fee76
aditya <122526560+aditya1183@users.noreply.github.com>
2025-01-25 22:42:02 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://real-estate-ten-blush.vercel.app/",
         secure: false,
       },
     },
